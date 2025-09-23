2e9145ca48c972c1edda61257780f703b2a241de
Shivanshu Prajapati <shivanshuofficial123@gmail.com>
2025-09-23 14:35:23 +0530

    Prepare frontend for production build

--- Client/src/Components/Api/Api.jsx
+++ Client/src/Components/Api/Api.jsx
@@ -1,9 +1,18 @@
+// import axios from "axios";
+
+// const api= axios.create({
+//     baseURL:import.meta.env.VITE_API_URL, // backend ka base url
+// });
+
+// export const Api={
+//   createContact:(formData)=>api.post("/contactRoute/insert",formData)
+// }
 import axios from "axios";
 
-const api= axios.create({
-    baseURL:import.meta.env.VITE_API_URL, // backend ka base url
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL // e.g. http://localhost:4000 or https://aruhkoncepts.onrender.com
 });
 
-export const Api={
-  createContact:(formData)=>api.post("/contactRoute/insert",formData)
-}
+export const Api = {
+  createContact: (formData) => api.post("/contactRoute/insert", formData)
+};
