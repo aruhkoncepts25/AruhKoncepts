be24c8acf3a991c12aaeba023b4cf1eaee38266c
aruhkoncepts25 <aruhkoncepts25@gmail.com>
2025-11-12 19:40:24 +0530

    Update Home.jsx for making less opacity

--- Client/src/Components/Home.jsx
+++ Client/src/Components/Home.jsx
@@ -768,7 +768,7 @@ useEffect(() => {
   <div className="fixed inset-0 z-[100] flex items-center justify-center p-4">
     {/* Full Screen Overlay */}
     <div 
-      className="absolute inset-0 bg-black/60 backdrop-blur-sm"
+      className="absolute inset-0 bg-black/60 backdrop-blur-xs"
       onClick={() => setIsOpen(false)}
     />
     
